e861bc0f972b03a5883a5887eb2f26e46b11a0f0
rohit chand <rohitchand010904@gmail.com>
2024-08-19 01:27:29 +0530

    seekbar

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -33,10 +33,39 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false);
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const previous = async () => {
+        if (track.id >0) {
+            await setTrack(songsData[track.id - 1]);
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+        const next = async () => {
+            if (track.id < songsData.length-1) {
+                await setTrack(songsData[track.id +1]);
+                await audioRef.current.play();
+                setPlayStatus(true);
+            }
+
+        }
+
+        const seekSong =async(e)=>{
+            console.log(e);
+
+        }
+    
     useEffect(() => {
         setTimeout(() => {
-            setTime(
-                {
+            audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%"
+                setTime({
                     currentTime: {
                         seconds: Math.floor(audioRef.current.currentTime % 60),
                         minutes: Math.floor(audioRef.current.currentTime / 60)
@@ -45,8 +74,8 @@ const PlayerContextProvider = (props) => {
                         seconds: Math.floor(audioRef.current.duration % 60),
                         minutes: Math.floor(audioRef.current.duration / 60)
                     }
-                }
-            )
+                })
+            }
 
         }, 1000);
 
@@ -62,6 +91,10 @@ const PlayerContextProvider = (props) => {
         playStatus, setPlayStatus,
         time, setTime,
         play, pause,
+        playWithId,
+        previous,
+        next,
+        seekSong
 
     }
 
